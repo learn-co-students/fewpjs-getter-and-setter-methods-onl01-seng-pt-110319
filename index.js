2f6e654db0d9854056a49490528b6400630636a9
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 06:20:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your Circle class here
+class Circle{
+  constructor(raius){
+    this.radius = radius;
+  }
+  
+  get diameter(){}
+  get circumference(){}
+  get area(){}
+  
+  
+  
+  
+}
